887462b06c3fd5e06628d368a4d413f13965ca2f
koisose.eth <plokotokbelajar@gmail.com>
2024-02-17 12:44:12 +0700

    change

--- src/utils/bullmq/queue-fix.ts
+++ src/utils/bullmq/queue-fix.ts
@@ -2,14 +2,26 @@
 import { Queue } from "bullmq";
 import { CONNECTOR, DEFAULT_REMOVE_CONFIG } from "./config";
 import setUpWorker from "./worker";
-import Redis from "ioredis";
+import {Redis} from "ioredis";
+let ready=false;
+// @ts-ignore
+let myQueue;
 
-const connection = new Redis(CONNECTOR as any);
-const myQueue = new Queue("JOBS", { connection });
+// @ts-ignore
+const addJobToQueue = (data) => {
+     // @ts-ignore
+
+  if(ready){
+    // @ts-ignore
+    return myQueue.add(data.jobName, data, DEFAULT_REMOVE_CONFIG);
+  }else{
+    const connection = new Redis(CONNECTOR as any);
+myQueue = new Queue("JOBS", { connection });
 myQueue.on("error", () => {
   connection.disconnect();
 });
 connection.on("connect", function () {
+     // @ts-ignore
   myQueue.setMaxListeners(Number(myQueue.getMaxListeners()) + 100);
   setUpWorker();
   console.log("Redis client connected");
@@ -19,11 +31,10 @@ connection.on("error", function () {
   //  console.log('Something went wrong ' + err);
   connection.disconnect();
 });
-
-// @ts-ignore
-const addJobToQueue = (data) => {
-  //  return {data,DEFAULT_REMOVE_CONFIG}
-  return myQueue.add(data.jobName, data, DEFAULT_REMOVE_CONFIG);
+ready=true;
+return myQueue.add(data.jobName, data, DEFAULT_REMOVE_CONFIG);
+  }
+  
 };
 
 export default addJobToQueue;
